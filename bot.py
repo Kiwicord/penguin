61f8be4f924585bf67d0f9636fc91410855aafa5
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-04-26 20:19:51 +0200

    Add invite command

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import hikari
 import lightbulb
+import miru
+import datetime
+
 from os import getenv
 from dotenv import load_dotenv
 
@@ -7,12 +10,29 @@ load_dotenv()
 
 bot = lightbulb.BotApp(
     token=getenv('TOKEN'),
-    prefix='-',
+    prefix='-', # idk why i put that here if the bot only uses slash cmds but hey
     default_enabled_guilds=(958713468624728114)
 )
 
-@bot.listen(hikari.StartedEvent)
-async def started(event):
-    print('bot has started')
+miru.load(bot)
+
+@bot.command
+@lightbulb.command(name='invite', description='Invite Penguin to your discord server.')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def invite_cmd(ctx):
+    embed = (
+        hikari.Embed(
+            title='Thank you for choosing the best penguin experience',
+            color=0xffffff,
+            timestamp=datetime.datetime.now().astimezone()
+        )
+    )
+
+    button = miru.Button(label='Get started', style=hikari.ButtonStyle.LINK, url='https://discord.com/api/oauth2/authorize?client_id=968510806738223154&permissions=8&scope=bot%20applications.commands')
+    
+    view = miru.View()
+    view.add_item(button)
+
+    await ctx.respond(embed, components=view.build())
 
 bot.run()
