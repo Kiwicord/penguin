4ba31ae8e895d164f7a89a1db021e982f07bde0a
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-04-26 20:23:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ load_dotenv()
 
 bot = lightbulb.BotApp(
     token=getenv('TOKEN'),
-    prefix='-', # idk why i put that here if the bot only uses slash cmds but hey
-    default_enabled_guilds=(958713468624728114)
+    prefix=lightbulb.when_mentioned_or('-'), # idk why i put that here if the bot only uses slash cmds but hey
+    default_enabled_guilds=(958713468624728114),
 )
 
 miru.load(bot)
