e59e612d479b0c599dbbe46d957d4de990218ab0
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-05-11 14:42:55 +0200

    Fix hikari.message context

--- core/help.py
+++ core/help.py
@@ -10,49 +10,43 @@ class HelpView(miru.View):
         placeholder='Select topic',
         options=[
             miru.SelectOption(label='Fun', emoji='🧩'),
-            miru.SelectOption(label='Music', emoji='🎶'),
-            miru.SelectOption(label='Moderation', emoji='🔧'),
-            miru.SelectOption(label='Economy', emoji='💰')
+            # miru.SelectOption(label='Music', emoji='🎶'),
+            # miru.SelectOption(label='Moderation', emoji='🔧'),
+            # miru.SelectOption(label='Economy', emoji='💰')
         ]
     )
     async def help_select(self, select_menu, ctx):
         value = select_menu.values[0]
-
-        if value == 'Moderation':
-            moderation_embed = (
+        
+        if value == 'Fun':
+            fun_embed = (
                 hikari.Embed(
-                    title='Moderation Commands',
-                    description='Mod commands LOL',
+                    title='🧩 Fun Commands',
                     color=0xffffff
                 )
+                .add_field(name='/penguin', value='Sends a random image of a penguin')
             )
+            dm_channel = await ctx.user.fetch_dm_channel()
+            await dm_channel.send(fun_embed)
 
-            dm_channel = await ctx.author.fetch_dm_channel()
-            await dm_channel.send(moderation_embed)
-
-@plugin.listener(hikari.GuildMessageCreateEvent)
-async def help_cmd(event):
-    if event.is_bot or not event.content:
-        return
-
-    if event.content == 'allah':
-        embed = (
-            hikari.Embed(
-                title='Bot Commands',
-                description='Click any button to get all the commands of a topic.',
-                color=0xffffff,
-                timestamp=datetime.datetime.now().astimezone()
-            )
-            .set_footer(f'Requested by {event.message.author}')
+@plugin.command
+@lightbulb.command('help', 'Sends bot help')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def help_cmd(ctx):
+    embed = (
+        hikari.Embed(
+            title='Bot Commands',
+            description='Click any button to get all the commands of a topic.',
+            color=0xffffff,
+            timestamp=datetime.datetime.now().astimezone()
         )
-
-        view = HelpView()
-
-        msg = await event.message.respond(embed, components=view.build())
-
-        view.start(message=msg)
-        await view.wait()
-        await event.message.respond('view timed out loil')
+        .set_footer(f'Requested by {ctx.author}')
+    )
+    view = HelpView()
+    msg = await ctx.respond(embed, components=view.build())
+    view.start(message=await msg.message())
+    await view.wait()
+    await ctx.respond('view timed out loil')
 
 def load(bot):
     bot.add_plugin(plugin)
